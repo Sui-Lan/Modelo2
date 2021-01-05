d76453b82703760bc40b7fd3f657e16d2782dc3d
Sui <sgonzalezmac@gmail.com>
2021-01-05 19:29:29 -0300

    comentario de prueba

--- 5.1.4_Modelo2/src/main/java/com/repaso/modelo/Usuario.java
+++ 5.1.4_Modelo2/src/main/java/com/repaso/modelo/Usuario.java
@@ -14,7 +14,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Usuario {
-
+//prueba 
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer idUsuario;
